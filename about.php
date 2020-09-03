0cab9d16282beb10306c88f6712362a3e68ba8a4
Riyabadoni28 <70255901+Riyabadoni28@users.noreply.github.com>
2020-09-03 03:12:43 -0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Riya badoni</title>
+
+  <!-- CSS only -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+  <!-- JS, Popper.js, and jQuery -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <!-- font awsome cdn -->
+  <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+
+  <link rel="stylesheet" href="about.css">
+</head>
+
+<body>
+ <section id="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-----<a class="navbar-brand" href="file:///C:/Users/deepak/Desktop/riya/index.html"><img src="img/logo2.jpg"></a>--->
+          <a class="navbar-brand font-weight-bold text-white p-2" href="file:///C:/Users/deepak/Desktop/riya/index.html"><b>EASYWEBLEARNING</b></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="service.php">services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog/blog.php">Blogs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About Me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+</section>
+  
+<div class="about-section">
+  <h1>About Us Page</h1>
+  <p>Our team has best web designer, best graphic designer. Our team is hard working and honestly do work. This are team working web site.
+  Our web site is fee to use in any place and very easy to use. </p>
+</div>
+
+<h2 style="text-align:center; margin-top: 10vh;">Our Team</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="img/user2.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Riya Badoni</h2>
+        <p class="title">Founder</p>
+        <p>Create a Design of website and leader of the team.
+        Currently living Rishikesh Uttarakhand India. I am BCA Student in Uttarakhand.
+            I do web designing using basic languages like HTML,CSS,Js and PHP. I do my work  proffesionaly and honestly.</p>
+        <p><b>badoniriya33@gmail.com</b></p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="img/user2.png" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Shweta Naithani</h2>
+        <p class="title">Co - Founder</p>
+        <p>Create a Graphic of website and second leader of website.
+         Currently living Rishikesh Uttarakhand India. I am BCA Student in Uttarakhand.
+         I do web designing using basic languages like HTML,CSS,Js and PHP. I do my work proffesionaly and honestly.</p>
+        <p>email</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="img/user1.jpg" alt="Jane" style="width:80%">
+      <div class="container">
+        <h2>Deepak Rawat</h2>
+        <p class="title">Helper</p>
+        <p>Help for creating web site and given ideas to create expensive website.
+         Currently living Rishikesh Uttarakhand India. I am BSc(IT) Student in Uttarakhand.
+            I do web designing using basic languages like HTML,CSS,Js and PHP. I do my work honestly.</p>
+        <p>email</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+  
+  </body>
+</html>
